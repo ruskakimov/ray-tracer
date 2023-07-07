f7c25221568252732d90717e21936891d63cc2f3
Rustem Kakimov <ruskakimov@gmail.com>
2023-07-07 21:57:29 +0600

    Create sphere

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils/image.h"
+#include "utils/math.h"
 
 int main() {
-  ImageHandle img = image_make(256, 256);
+  ImageHandle img = image_make(400, 300);
+  Sphere sphere = { .center = { 0.0, 0.0, -20.0 }, .radius = 8.0 };
 
   for (int r = 0; r < img.height; r++) {
     for (int c = 0; c < img.width; c++) {
+      // shoot ray from 0,0,0 through pixel
+      // color red if hit
       uint8_t red = 255 * c / img.width;
       uint8_t green = 255 * r / img.height;
       Pixel pixel = { red, green, 64 };
