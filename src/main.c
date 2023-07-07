f7ceba91283ba4199cd018815b516a7e0e13cfee
Rustem Kakimov <ruskakimov@gmail.com>
2023-07-07 14:41:48 +0600

    Render gradient

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "image.h"
+#include "utils/image.h"
 
 int main() {
-  ImageHandle img_handle = image_make(100, 100);
-  image_printAsPPM(&img_handle);
-  free(img_handle.pixels);
+  ImageHandle img = image_make(256, 256);
+
+  for (int r = 0; r < img.height; r++) {
+    for (int c = 0; c < img.width; c++) {
+      uint8_t red = 255 * c / img.width;
+      uint8_t green = 255 * r / img.height;
+      Pixel pixel = { red, green, 64 };
+      *(img.pixels + r * img.width + c) = pixel;
+    }
+  }
+
+  image_printAsPPM(&img);
+  free(img.pixels);
 }
