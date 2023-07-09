b1dda4217d25528ffcb8e897b4c984549d77cdb5
Rustem Kakimov <ruskakimov@gmail.com>
2023-07-09 15:28:23 +0600

    Add vector class

---
+++ src/utils/Vec3.h
@@ -0,0 +1,24 @@
+#pragma once               
+
+class Vec3 {
+private:
+  double m_x, m_y, m_z;
+public:
+  Vec3(double x, double y, double z);
+
+  double x() const { return m_x; };
+  double y() const { return m_y; };
+  double z() const { return m_z; };
+
+  double sqLen() const;
+  double len() const;
+
+  Vec3& operator+=(const Vec3& v);
+  Vec3& operator-=(const Vec3& v);
+  Vec3& operator*=(const double t);
+  Vec3& operator/=(const double t);
+};
+
+Vec3 operator-(Vec3 a);
+Vec3 operator+(Vec3 a, const Vec3& b);
+Vec3 operator-(Vec3 a, const Vec3& b);
--- src/utils/math.h
+++ src/utils/math.h
@@ -5,29 +5,29 @@ typedef struct {
   double y;
 } Vec2;
 
-typedef struct {
-  double x;
-  double y;
-  double z;
-} Vec3;
+// typedef struct {
+//   double x;
+//   double y;
+//   double z;
+// } Vec3;
 
-typedef struct {
-  Vec3 origin;
-  Vec3 dir;
-} Ray;
+// typedef struct {
+//   Vec3 origin;
+//   Vec3 dir;
+// } Ray;
 
-typedef struct {
-  Vec3 center;
-  double radius;
-} Sphere;
+// typedef struct {
+//   Vec3 center;
+//   double radius;
+// } Sphere;
 
-Vec3 vec3_add(Vec3 v1, Vec3 v2);
-Vec3 vec3_sub(Vec3 v1, Vec3 v2);
-Vec3 vec3_scale(Vec3 v1, double scalar);
-double vec3_sqLen(Vec3 v);
+// Vec3 vec3_add(Vec3 v1, Vec3 v2);
+// Vec3 vec3_sub(Vec3 v1, Vec3 v2);
+// Vec3 vec3_scale(Vec3 v1, double scalar);
+// double vec3_sqLen(Vec3 v);
 
-Vec3 ray_pointAt(Ray ray, double time);
-double ray_sphereHitTime(Ray ray, Sphere sphere);
+// Vec3 ray_pointAt(Ray ray, double time);
+// double ray_sphereHitTime(Ray ray, Sphere sphere);
 
-double calcDiscriminant(double a, double b, double c);
-Vec2 calcRoots(double a, double b, double discriminant);
+// double calcDiscriminant(double a, double b, double c);
+// Vec2 calcRoots(double a, double b, double discriminant);
